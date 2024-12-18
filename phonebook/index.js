cbf4ee0bb5063d304a0e408c4f11210478bcc3ad
manosmin <manosmin@hotmail.com>
2024-12-18 06:04:46 +0200

    part3 exercises

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons =[
     { 
       "id": "1",
@@ -26,6 +28,60 @@ let persons =[
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const note = persons.find(note => note.id === id)
+  
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+
+  if (!request.body.name || !request.body.number) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  if (persons.find((p) => p.name === request.body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
+  const person = {
+    name: request.body.name,
+    number: request.body.number,
+    id: generateId(),
+  }
+  
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => Number(p.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.get('/api/info', (request, response) => {
+  response.send(`Phonebook has info for ${persons.length} people <br/> ${new Date()}`)
 })
 
 const PORT = 3001
