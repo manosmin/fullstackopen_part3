e6b6ca7452144a274bc5e871e6dac83066ffb243
manosmin <manosmin@hotmail.com>
2024-12-21 00:50:33 +0200

    part3 exercises

---
+++ mongo.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://man0sm1n96:${password}@cluster0.nsr3e.mongodb.net/`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+})
+
+person.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
